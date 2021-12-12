939545f00b2ccabaaf3c86f01bdcac32f9ec14ba
Dion Bosschieter <dbosschieter@transip.nl>
2021-12-12 16:58:34 +0100

    Refresh tokens on error

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -ex
 
-go build -o downloader main.go
-
-# create appropriate folder for plugins
-fpm -f -s dir -t deb -n godownloader downloader=/usr/local/bin/
+CGO_ENABLED=0 go build -o downloader main.go
--- searchprovider/rarbg/rarbg.go
+++ searchprovider/rarbg/rarbg.go
@@ -1,42 +1,48 @@
 package rarbg
 
 import (
-    rar "github.com/ricksancho/rarbg-torrentapi"
+	rar "github.com/ricksancho/rarbg-torrentapi"
+	"log"
 )
 
 type SearchProvider struct {
-    client *rar.Client
+	client *rar.Client
 }
 
 func (provider *SearchProvider) Name() string {
-    return "rarbg"
+	return "rarbg"
 }
 
 func (provider *SearchProvider) Search(title string, searchPostfixes []string) (magnet string) {
-    // query with the provided postfixes
-    for _, searchPostfix := range searchPostfixes {
-        var query = map[string]string{"search_string":title + " " + searchPostfix, "sort": "seeders"}
+	// query with the provided postfixes
+	for _, searchPostfix := range searchPostfixes {
+		var query = map[string]string{"search_string": title + " " + searchPostfix, "sort": "seeders"}
 
-        result,_ := provider.client.Search(query)
+		result, err := provider.client.Search(query)
+		if err != nil {
+			log.Printf("rarbgto error occured: '%s'\n", err.Error())
+			provider.client.AppId++
+			_ = provider.client.GetToken() // refresh the token just in case
+		}
 
-        if len(result.Torrents) > 0 {
-            return result.Torrents[0].MagnetURL
-        }
-    }
+		if len(result.Torrents) > 0 {
+			return result.Torrents[0].MagnetURL
+		}
+	}
 
-    // fall back to postfix less search
-    var query = map[string]string{"search_string":title, "sort": "seeders"}
+	// fall back to postfix less search
+	var query = map[string]string{"search_string": title, "sort": "seeders"}
 
-    result,_ := provider.client.Search(query)
+	result, _ := provider.client.Search(query)
 
-    if len(result.Torrents) > 0 {
-        return result.Torrents[0].MagnetURL
-    }
+	if len(result.Torrents) > 0 {
+		return result.Torrents[0].MagnetURL
+	}
 
-    return ""
+	return ""
 }
 
 func (provider *SearchProvider) Init() {
-    provider.client,_ = rar.New(1337)
-    provider.client.Init()
+	provider.client, _ = rar.New(1337)
+	_ = provider.client.Init()
 }
